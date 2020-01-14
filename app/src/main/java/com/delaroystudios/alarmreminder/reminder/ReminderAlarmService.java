d509fd8840c8ed880a35010f67e1c421534f505c
Adam <51300654+AdamKaim@users.noreply.github.com>
2020-01-14 03:18:16 +0100

    Przypominajka Dodanie obrazów(minimap, drawable), colorów, menu, desain dla aplikacji cały activity_add_reminder, activity_main, alarm_item nowe implementacje Cała składania Class z odliczaniem i powiadomieniem

---
+++ app/src/main/java/com/delaroystudios/alarmreminder/reminder/ReminderAlarmService.java
@@ -0,0 +1,70 @@
+package com.delaroystudios.alarmreminder.reminder;
+
+import android.app.IntentService;
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.database.Cursor;
+import android.media.RingtoneManager;
+import android.net.Uri;
+
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.TaskStackBuilder;
+
+import com.delaroystudios.alarmreminder.AddReminderActivity;
+import com.delaroystudios.alarmreminder.R;
+import com.delaroystudios.alarmreminder.data.AlarmReminderContract;
+
+public class ReminderAlarmService extends IntentService {
+    private static final String TAG = ReminderAlarmService.class.getSimpleName();
+    private NotificationCompat.Builder notificationBuilder;
+    private static final int NOTIFICATION_ID = 42;
+
+    Cursor cursor;
+    public static PendingIntent getReminderPendingIntent(Context context, Uri uri) {
+        Intent action = new Intent(context, ReminderAlarmService.class);
+        action.setData(uri);
+        return PendingIntent.getService(context, 0, action, PendingIntent.FLAG_UPDATE_CURRENT);
+    }
+
+    public ReminderAlarmService() {
+        super(TAG);
+    }
+
+    @Override
+    protected void onHandleIntent(Intent intent) {
+        NotificationManager manager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+        Uri uri = intent.getData();
+        Intent action = new Intent(this, AddReminderActivity.class);
+        action.setData(uri);
+        PendingIntent operation = TaskStackBuilder.create(this)
+                .addNextIntentWithParentStack(action)
+                .getPendingIntent(0, PendingIntent.FLAG_UPDATE_CURRENT);
+        if(uri != null){
+            cursor = getContentResolver().query(uri, null, null, null, null);
+        }
+        String description = "";
+        try {
+            if (cursor != null && cursor.moveToFirst()) {
+                description = AlarmReminderContract.getColumnString(cursor, AlarmReminderContract.AlarmReminderEntry.KEY_TITLE);
+            }
+        } finally {
+            if (cursor != null) {
+                cursor.close();
+            }
+        }
+         notificationBuilder = new NotificationCompat.Builder(this)
+                .setContentTitle(getString(R.string.reminder_title))
+                .setContentText(description)
+                .setSmallIcon(R.drawable.ic_add_alert_black_24dp)
+                .setContentIntent(operation)
+                .setAutoCancel(true);
+        long[] v = {500, 1000};
+         Uri uriSound = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
+        notificationBuilder.setVibrate(v);
+        notificationBuilder.setSound(uriSound);
+        manager.notify(NOTIFICATION_ID, notificationBuilder.build());
+    }
+}
